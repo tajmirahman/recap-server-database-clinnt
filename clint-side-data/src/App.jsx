06281f0529ec2135a26b2eac9c8b41d8ce9685b5
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-29 00:09:28 +0600

    from work done

--- clint-side-data/src/App.jsx
+++ clint-side-data/src/App.jsx
@@ -3,11 +3,27 @@ import './App.css'
 
 function App() {
 
+  const handleAddUser=(e)=>{
+    e.preventDefault();
+    const from=e.target;
+    const name=from.name.value;
+    const email=from.email.value;
+    const user={name,email}
+    console.log(user)
+  }
 
   return (
     <>
       
       <h1>Recap Clint side server</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="add User" />
+      </form>
       
     </>
   )
